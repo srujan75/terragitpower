d9e3aa12799eed6680f08afcb0ee665c7afebe5f
srujan75 <srujanneeruganti@gmail.com>
2025-04-12 17:30:22 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# Set variables
-resourceGroup="terraform-state-rg"
-storageAccount="tfstatestorage1234"
-containerName="tfstate"
-
-# Create resource group and storage
-az group create --name $resourceGroup --location eastus
-az storage account create --name $storageAccount --resource-group $resourceGroup --location eastus --sku Standard_LRS
-
-# Get storage key and create container
-accountKey=$(az storage account keys list --resource-group $resourceGroup --account-name $storageAccount --query '[0].value' -o tsv)
-az storage container create --name $containerName --account-name $storageAccount --account-key $accountKey
-
 provider "azurerm" {
   features {}
 
